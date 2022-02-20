87874bbfd568ead4af523b64aa61f1381638913e
bitbof <bitbof@gmail.com>
2022-02-20 18:35:09 +0100

    optimize smudge, smudge sharper for small brush size

--- src/app/script/klecks/brushes/smudge-brush.ts
+++ src/app/script/klecks/brushes/smudge-brush.ts
@@ -17,31 +17,57 @@ import {IVector2D} from '../../bb/bb.types';
 // ix, iy + sans rounding + fast random + offset const: 0.48 ms
 
 
+
+interface IBounds {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+}
+
+interface ISmudgeParams {
+    aP: IVector2D;
+    bP: IVector2D;
+    size: {
+        w: number;
+        h: number;
+    };
+    brush: {
+        center: IVector2D,
+        size: number;
+        opacity: number;
+        alphaLock: boolean;
+    };
+}
+
+
 /**
- * Pixel operations that do the smudging via ImageData
- * @param imageData
+ * determine bounds of smudge
+ * @param imWidth
+ * @param imHeight
  * @param aP
  * @param bP
  * @param size
- * @param options
  */
-function smudge(
-    imageData: ImageData,
+function prepSmudge(
+    imWidth: number,
+    imHeight: number,
     aP: IVector2D, // top left of source, integers
     bP: IVector2D, // top left of dest, integers
     size: {w: number; h: number}, // size of both rectangles, integers
-    options: {
-        opacity: number; // 0 - 1 float
-        alphaLock: boolean;
-    }
-): IBounds {
+): {
+    aP: IVector2D,
+    bP: IVector2D,
+    size: {w: number; h: number},
+} {
+
     if (aP.x === bP.x && aP.y === bP.y) {
         return null;
     }
 
-    const cSize = size.w / 2;
-    const cX = aP.x + size.w / 2;
-    const cY = aP.y + size.h / 2;
+    aP = BB.copyObj(aP);
+    bP = BB.copyObj(bP);
+    size = BB.copyObj(size);
 
     // corner behavior
     // cut down rectangles, if a or b go outside
@@ -64,17 +90,17 @@ function smudge(
         if (bP.y < 0) {
             top = Math.max(top, -bP.y);
         }
-        if (aP.x + size.w > imageData.width) {
-            right = aP.x + size.w - imageData.width;
+        if (aP.x + size.w > imWidth) {
+            right = aP.x + size.w - imWidth;
         }
-        if (aP.y + size.h > imageData.height) {
-            bottom = aP.y + size.h - imageData.height;
+        if (aP.y + size.h > imHeight) {
+            bottom = aP.y + size.h - imHeight;
         }
-        if (bP.x + size.w > imageData.width) {
-            right = Math.max(right, bP.x + size.w - imageData.width);
+        if (bP.x + size.w > imWidth) {
+            right = Math.max(right, bP.x + size.w - imWidth);
         }
-        if (bP.y + size.h > imageData.height) {
-            bottom = Math.max(bottom, bP.y + size.h - imageData.height);
+        if (bP.y + size.h > imHeight) {
+            bottom = Math.max(bottom, bP.y + size.h - imHeight);
         }
 
         aP.x += left;
@@ -89,6 +115,36 @@ function smudge(
         }
     }
 
+    return {
+        aP: {
+            x: aP.x,
+            y: aP.y,
+        },
+        bP: {
+            x: bP.x,
+            y: bP.y,
+        },
+        size: {
+            w: size.w,
+            h: size.h,
+        }
+    };
+}
+
+
+/**
+ * Pixel operations that do the smudging via ImageData
+ * @param imageData
+ * @param p
+ */
+function smudge(imageData: ImageData, p : ISmudgeParams) {
+
+    p = BB.copyObj(p);
+
+    const cSize = p.brush.size;
+    const cX = p.brush.center.x;
+    const cY = p.brush.center.y;
+
     /*if (statCount % 1000 === 0) {
         console.log(statAcc / 1000);
         statAcc = 0;
@@ -97,8 +153,8 @@ function smudge(
 
 
     // determine offset
-    let aIndex = aP.y * imageData.width + aP.x;
-    let bIndex = bP.y * imageData.width + bP.x;
+    let aIndex = p.aP.y * imageData.width + p.aP.x;
+    let bIndex = p.bP.y * imageData.width + p.bP.x;
     const offset = (bIndex - aIndex) * 4;
 
     // array with random numbers. faster than Math.random()
@@ -109,12 +165,12 @@ function smudge(
         randArr[i] = Math.random();
     }
 
-    const softness = 8;
+    const softness = Math.max(3, Math.min(8, p.brush.size - 8));
 
     const pixel = (ai, bi, ix, iy) => {
 
         const dist = Math.abs(BB.dist(cX, cY, ix, iy));
-        const fac = 1 - options.opacity * (1 - BB.clamp((dist - (cSize - softness)) / softness, 0, 1));
+        const fac = 1 - p.brush.opacity * (1 - BB.clamp((dist - (cSize - softness)) / softness, 0, 1));
 
         if (fac === 1) {
             return;
@@ -145,65 +201,51 @@ function smudge(
             imageData.data[bi + 2] = Math.floor(BB.mix(imageData.data[ai + 2], imageData.data[bi + 2], fac2) + randArr[randI++ % randLen]);
         }
         // Always mix alpha. unless alpha lock
-        if (!options.alphaLock) {
+        if (!p.brush.alphaLock) {
             imageData.data[bi + 3] = Math.floor(BB.mix(imageData.data[ai + 3], imageData.data[bi + 3], fac) + 0.5);
         }
 
     };
 
-    const bx1 = bP.x * 4;
-    const bx2 = bx1 + (size.w - 1) * 4;
+    const bx1 = p.bP.x * 4;
+    const bx2 = bx1 + (p.size.w - 1) * 4;
 
     // transfer of pixels depends on direction of smudging if there is overlap
-    if (aP.y < bP.y) {
-        for (let y = size.h - 1, iy = bP.y + size.h - 1; y >= 0; y--, iy--) {
-            const yStart = (y + bP.y) * imageData.width * 4;
-            for (let x = bx2 + yStart, x2 = bx2 + yStart - offset, ix = bP.x + size.w - 1; x >= bx1 + yStart; x -= 4, x2 -= 4, ix--) {
+    if (p.aP.y < p.bP.y) {
+        for (let y = p.size.h - 1, iy = p.bP.y + p.size.h - 1; y >= 0; y--, iy--) {
+            const yStart = (y + p.bP.y) * imageData.width * 4;
+            for (let x = bx2 + yStart, x2 = bx2 + yStart - offset, ix = p.bP.x + p.size.w - 1; x >= bx1 + yStart; x -= 4, x2 -= 4, ix--) {
                 pixel(x2, x, ix, iy);
             }
         }
-    } else if (aP.y > bP.y) {
-        for (let y = 0, iy = bP.y; y < size.h; y++, iy++) {
-            const yStart = (y + bP.y) * imageData.width * 4;
-            for (let x = bx2 + yStart, x2 = bx2 + yStart - offset, ix = bP.x + size.w - 1; x >= bx1 + yStart; x -= 4, x2 -= 4, ix--) {
+    } else if (p.aP.y > p.bP.y) {
+        for (let y = 0, iy = p.bP.y; y < p.size.h; y++, iy++) {
+            const yStart = (y + p.bP.y) * imageData.width * 4;
+            for (let x = bx2 + yStart, x2 = bx2 + yStart - offset, ix = p.bP.x + p.size.w - 1; x >= bx1 + yStart; x -= 4, x2 -= 4, ix--) {
                 pixel(x2, x, ix, iy);
             }
         }
     } else {
-        if (aP.x < bP.x) {
-            for (let y = size.h - 1, iy = bP.y + size.h - 1; y >= 0; y--, iy--) {
-                const yStart = (y + bP.y) * imageData.width * 4;
-                for (let x = bx2 + yStart, x2 = bx2 + yStart - offset, ix = bP.x + size.w - 1; x >= bx1 + yStart; x -= 4, x2 -= 4, ix--) {
+        if (p.aP.x < p.bP.x) {
+            for (let y = p.size.h - 1, iy = p.bP.y + p.size.h - 1; y >= 0; y--, iy--) {
+                const yStart = (y + p.bP.y) * imageData.width * 4;
+                for (let x = bx2 + yStart, x2 = bx2 + yStart - offset, ix = p.bP.x + p.size.w - 1; x >= bx1 + yStart; x -= 4, x2 -= 4, ix--) {
                     pixel(x2, x, ix, iy);
                 }
             }
         } else {
-            for (let y = 0, iy = bP.y; y < size.h; y++, iy++) {
-                const yStart = (y + bP.y) * imageData.width * 4;
-                for (let x = bx1 + yStart, x2 = bx1 + yStart - offset, ix = bP.x; x < bx2 + yStart; x += 4, x2 += 4, ix++) {
+            for (let y = 0, iy = p.bP.y; y < p.size.h; y++, iy++) {
+                const yStart = (y + p.bP.y) * imageData.width * 4;
+                for (let x = bx1 + yStart, x2 = bx1 + yStart - offset, ix = p.bP.x; x < bx2 + yStart; x += 4, x2 += 4, ix++) {
                     pixel(x2, x, ix, iy);
                 }
             }
         }
     }
 
-    /*statCount++;
-    statAcc += performance.now() - start;*/
-
-    return {
-        x1: bP.x,
-        y1: bP.y,
-        x2: bP.x + size.w,
-        y2: bP.y + size.h
-    };
+    //statCount++;
+    //statAcc += performance.now() - start;
 }
-
-interface IBounds {
-    x1: number;
-    y1: number;
-    x2: number;
-    y2: number;
-};
 
 /**
  * Brush that pushes colors around.
@@ -212,7 +254,7 @@ export function smudgeBrush() {
 
     let _this = this;
     let debugStr = '';
-    let context;
+    let context: CanvasRenderingContext2D;
     let history = {
         add: function (p?) {
         },
@@ -220,7 +262,7 @@ export function smudgeBrush() {
     }, historyEntry;
 
     let settingColor, settingSize = 35, settingSpacing = 0.20446882736951905, settingOpacity = 0.8;
-    let settingColorStr;
+    let settingColorStr: string;
     let settingHasSizePressure = false, settingHasOpacityPressure = false;
     let settingLockLayerAlpha = false;
 
@@ -236,8 +278,11 @@ export function smudgeBrush() {
     let redrawBounds: IBounds;
     let completeRedrawBounds: IBounds;
 
-    let copyImageData;
+    let copyImageData: ImageData;
 
+    let smudgeBuffer: ISmudgeParams[] = [];
+    const cellSize = 256;
+    let copiedCells: boolean[];
 
     function updateRedrawBounds(bounds: IBounds) {
         if (!bounds) {
@@ -265,6 +310,64 @@ export function smudgeBrush() {
 
 
     let lastDot;
+
+    /**
+     * update copyImageData. copy over new regions if needed
+     */
+    function copyFromCanvas() {
+
+        const touchedCells = copiedCells.map(item => false);
+
+        const bounds: IBounds[] = [];
+        const cellsW = Math.ceil(copyImageData.width / cellSize);
+
+        if (!redrawBounds) {
+            return;
+        }
+        bounds.push({
+            x1: Math.floor(redrawBounds.x1 / cellSize),
+            y1: Math.floor(redrawBounds.y1 / cellSize),
+            x2: Math.floor(redrawBounds.x2 / cellSize),
+            y2: Math.floor(redrawBounds.y2 / cellSize),
+        });
+        bounds.forEach(item => {
+            for (let i = item.x1; i <= item.x2; i++) {
+                for (let e = item.y1; e <= item.y2; e++) {
+                    touchedCells[e * cellsW + i] = true;
+                }
+            }
+        });
+
+        touchedCells.forEach((item, i) => {
+            if (!item || copiedCells[i]) {
+                // not touched, or already copied
+                return;
+            }
+            copiedCells[i] = true;
+            const x = i % cellsW;
+            const y = Math.floor(i / cellsW);
+            const w = (Math.min(x * cellSize + cellSize, copyImageData.width) - 1) % cellSize + 1;
+            const h = (Math.min(y * cellSize + cellSize, copyImageData.height) - 1) % cellSize + 1;
+
+            // temp canvas to prevent main canvas from getting slowed down in chrome
+            const tmpCanvas = BB.canvas(w, h);
+            const tmpCtx = tmpCanvas.getContext('2d');
+            tmpCtx.drawImage(context.canvas, -x * cellSize, -y * cellSize);
+
+            const data = tmpCtx.getImageData(0, 0, w, h);
+
+            for (let i = 0; i < h; i++) {
+                for (let e = 0, e2 = i * w * 4, e3 = ((y * cellSize + i) * copyImageData.width + x * cellSize) * 4; e < w; e++, e2 += 4, e3 += 4) {
+                    copyImageData.data[e3] = data.data[e2];
+                    copyImageData.data[e3 + 1] = data.data[e2 + 1];
+                    copyImageData.data[e3 + 2] = data.data[e2 + 2];
+                    copyImageData.data[e3 + 3] = data.data[e2 + 3];
+                }
+            }
+        });
+    }
+
+
     /**
      *
      *
@@ -288,8 +391,9 @@ export function smudgeBrush() {
         const w = Math.round(size * 2);
         const h = Math.round(size * 2);
 
-        const bounds = smudge(
-            copyImageData,
+        const bounds = prepSmudge(
+            copyImageData.width,
+            copyImageData.height,
             {
                 x: Math.round(lastDot.x - size),
                 y: Math.round(lastDot.y - size),
@@ -302,13 +406,28 @@ export function smudgeBrush() {
                 w,
                 h,
             },
-            {
-                opacity: opacity,
-                alphaLock: settingLockLayerAlpha,
-            }
         );
-        updateRedrawBounds(bounds);
 
+        if (bounds) {
+            const params: ISmudgeParams = {
+                aP: bounds.aP,
+                bP: bounds.bP,
+                size: bounds.size,
+                brush: {
+                    center: { x, y },
+                    size,
+                    opacity,
+                    alphaLock: settingLockLayerAlpha,
+                }
+            };
+            updateRedrawBounds({
+                x1: params.bP.x,
+                y1: params.bP.y,
+                x2: params.bP.x + params.brush.size * 2,
+                y2: params.bP.y + params.brush.size * 2,
+            });
+            smudgeBuffer.push(params);
+        }
 
         lastDot = {
             x: x,
@@ -317,6 +436,8 @@ export function smudgeBrush() {
     }
 
     function continueLine(x, y, size, pressure) {
+        smudgeBuffer = [];
+
         if(bezierLine === null) {
             bezierLine = new BB.BezierLine();
             bezierLine.add(lastInput.x, lastInput.y, 0, function(){});
@@ -339,14 +460,18 @@ export function smudgeBrush() {
         }
 
         // execute draw instructions
-        context.save();
         let before;
         for (let i = 0; i < drawArr.length; i++) {
             let item = drawArr[i];
             drawDot(item[0], item[1], item[2], item[3]);
             before = item;
         }
-        context.restore();
+
+        copyFromCanvas();
+
+        for (let i = 0; i < smudgeBuffer.length; i++) {
+            smudge(copyImageData, smudgeBuffer[i]);
+        }
     }
 
     //------------------ interface ---------------------------------------------------
@@ -365,7 +490,9 @@ export function smudgeBrush() {
         lastDot = null;
         isDrawing = true;
 
-        copyImageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+        copyImageData = new ImageData(context.canvas.width, context.canvas.height);
+        const totalCells = Math.ceil(context.canvas.width / cellSize) * Math.ceil(context.canvas.height / cellSize);
+        copiedCells = '0'.repeat(totalCells).split('').map(item => false);
 
         drawDot(x, y, localSize, localOpacity);
 
@@ -391,7 +518,7 @@ export function smudgeBrush() {
 
 
         if (redrawBounds) {
-            context.putImageData(copyImageData, 0, 0, redrawBounds.x1, redrawBounds.y1, redrawBounds.x2 - redrawBounds.x1, redrawBounds.y2 - redrawBounds.y1);
+            context.putImageData(copyImageData, 0, 0, redrawBounds.x1, redrawBounds.y1, redrawBounds.x2 - redrawBounds.x1 - 1, redrawBounds.y2 - redrawBounds.y1 - 1);
             updateCompleteRedrawBounds(redrawBounds.x1, redrawBounds.y1, redrawBounds.x2, redrawBounds.y2);
         }
 
@@ -399,7 +526,6 @@ export function smudgeBrush() {
         lastInput.y = y;
         lastInput2.pressure = lastInput.pressure;
         lastInput.pressure = pressure;
-
 
     };
 
@@ -415,18 +541,24 @@ export function smudgeBrush() {
         bezierLine = null;
 
         if (redrawBounds) {
-            context.putImageData(copyImageData, 0, 0, redrawBounds.x1, redrawBounds.y1, redrawBounds.x2 - redrawBounds.x1, redrawBounds.y2 - redrawBounds.y1);
+            context.putImageData(copyImageData, 0, 0, redrawBounds.x1, redrawBounds.y1, redrawBounds.x2 - redrawBounds.x1 - 1, redrawBounds.y2 - redrawBounds.y1 - 1);
             updateCompleteRedrawBounds(redrawBounds.x1, redrawBounds.y1, redrawBounds.x2, redrawBounds.y2);
         }
 
         if (historyEntry && completeRedrawBounds) {
             let historyImageData = copyImageData;
             if (!(completeRedrawBounds.x1 === 0 && completeRedrawBounds.y1 === 0 && completeRedrawBounds.x2 >= context.canvas.width - 1 && completeRedrawBounds.y2 >= context.canvas.height - 1)) {
-                historyImageData = context.getImageData(
-                    completeRedrawBounds.x1,
-                    completeRedrawBounds.y1,
-                    completeRedrawBounds.x2 - completeRedrawBounds.x1,
-                    completeRedrawBounds.y2 - completeRedrawBounds.y1
+
+                // temp canvas to prevent main canvas from getting slowed down in chrome
+                const tmpCanvas = BB.canvas(completeRedrawBounds.x2 - completeRedrawBounds.x1 + 1, completeRedrawBounds.y2 - completeRedrawBounds.y1 + 1);
+                const tmpCtx = tmpCanvas.getContext('2d');
+                tmpCtx.drawImage(context.canvas, -completeRedrawBounds.x1, -completeRedrawBounds.y1);
+
+                historyImageData = tmpCtx.getImageData(
+                    0,
+                    0,
+                    tmpCanvas.width,
+                    tmpCanvas.height,
                 );
             }
             historyEntry.actions.push({
