30feacea1c7b5a44962e0bc14b72e8bd2b302565
bitbof <bitbof@gmail.com>
2022-04-01 08:07:48 +0200

    minimal scrollTo polyfill

--- src/app/script/polyfills.ts
+++ src/app/script/polyfills.ts
@@ -1,2 +1,11 @@
 import 'core-js';
 import 'mdn-polyfills/Node.prototype.append';
+
+if (!('scrollTo' in Element.prototype)) {
+    Object.defineProperty(Element.prototype, 'scrollTo', {
+        value: function (x, y) {
+            this.scrollLeft = x;
+            this.scrollTop = y;
+        }
+    });
+}
