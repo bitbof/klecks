0f93e5aea91b1aa517f8c9472b84382c136bb141
bitbof <bitbof@gmail.com>
2022-04-21 17:23:18 +0200

    canvas debug code

--- src/app/script/bb/base/create-canvas.ts
+++ src/app/script/bb/base/create-canvas.ts
@@ -1,8 +1,22 @@
+/*window['canvases'] = [];
+
+window['printCanvases'] = () => {
+    let total = 0;
+
+    console.log(window['canvases'].map(item => {
+        total += item.width * item.height;
+        return {w: item.width, h: item.height};
+    }));
+
+    console.log('total: ' + (total * 4 / 1000 / 1000) + 'mb');
+};*/
+
 export function createCanvas(w?: number, h?: number): HTMLCanvasElement {
     const result = document.createElement('canvas');
     if (w && h) {
         result.width = w;
         result.height = h;
     }
+    // window['canvases'].push(result);
     return result;
 }
