7531e9c853527a0c12f15a6451661e96ae6ff9bc
Mathijs Wiermans <mathijswiermans@hotmail.com>
2024-08-25 00:41:16 +0200

    add styling

--- src/app/script/klecks/ui/modals/share-dialog.ts
+++ src/app/script/klecks/ui/modals/share-dialog.ts
@@ -4,7 +4,7 @@ import {ColorOptions} from '../components/color-options';
 import {showModal} from './base/showModal';
 import {LANG} from '../../../language/language';
 import {IRGB, IRGBA} from '../../kl-types';
-import {ISize2D} from '../../../bb/bb-types';
+import {IKeyString, ISize2D} from '../../../bb/bb-types';
 import {table} from '../components/table';
 import {theme} from '../../../theme/theme';
 import QRCode from 'qrcode';
@@ -13,21 +13,37 @@ export function shareDialog (
         image: Blob
     }
 ): void {
-
     const mainDiv = BB.el();
-    const canvas = BB.canvas(400, 400)
-    QRCode.toCanvas(canvas, "https://ai-image.manglemoose.com/result")
-    mainDiv.append(canvas)
+    const canvas = BB.canvas(800, 800)
+    QRCode.toCanvas(canvas, "https://ai-image.manglemoose.com/result", { width: 500})
+    canvas.style.marginLeft = 'auto';
+    canvas.style.marginRight = 'auto';
+    canvas.style.paddingLeft = '0';
+    canvas.style.paddingRight = '0';
+    canvas.style.display = 'block';
+
     var url = URL.createObjectURL(p.image)
     var image = new Image()
     image.src = url;
+    image.style.width = "100%"
+    image.style.marginLeft = 'auto';
+    image.style.marginRight = 'auto';
+    image.style.paddingLeft = '0';
+    image.style.paddingRight = '0';
+    image.style.display = 'block';
     mainDiv.append(image)
-
+    mainDiv.append(canvas)
     showModal({
         target: document.body,
         message: `<b>${LANG('share-title')}</b>`,
         div: mainDiv,
         buttons: ['Ok',],
+        style: {
+            width: 'calc(100% - 50px)',
+            maxWidth: '1000px',
+            minWidth: '300px',
+            boxSizing: 'border-box',
+        },
         callback: function (result) {
             if (
                 result === 'Cancel'
